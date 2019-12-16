477ad0b38b80dd9864cf23051f2750e3f6fa6fc4
Alex Huk <alex.huk85@gmail.com>
2019-12-16 20:20:36 +1100

    Add Deck

--- game.py
+++ game.py
@@ -5,6 +5,7 @@ ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten',
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
          'Queen':10, 'King':10, 'Ace':11}
 
+# class for single card object
 class Card():
 
     def __init__(self,suit,rank):
@@ -13,4 +14,25 @@ class Card():
 
     def __str__(self):
         return f'{self.rank} of {self.suit}'
+
+# class for deck object
+class Deck():
+
+    def __init__(self):
+        # list to hold 52 cards
+        self.deck = []
+
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit,rank))
+    
+    def __str__(self): # for display all cards
         
+        self.string = ''
+
+        for i in self.deck:
+            self.string += i.__str__()+'\n'
+        
+        return self.string
+
+print(Deck())
